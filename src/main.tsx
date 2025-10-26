08c465680ef0738fcd494d7df0448c38d95c79f9
simonteague6 <simon@dashf.com>
2025-10-26 10:58:01 -0500

    resolved merge conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./tailwind.css";
-import Game from "./Game.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Game />
+    <App />
   </StrictMode>
 );
