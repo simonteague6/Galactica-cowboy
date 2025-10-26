11b4e6a6eac20783696c3c2bd0a470e94c97ba3c
simonteague6 <simon@dashf.com>
2025-10-26 11:00:09 -0500

    actually resolved merge conflicts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SplitScreen from './components/Splitscreen'
+
+const App = () => {
+  return (
+    <div>
+        <SplitScreen></SplitScreen>
+    </div>
+  )
+}
+
+export default App
